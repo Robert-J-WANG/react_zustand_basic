8774bedf1169cc0425529f73980c9b31e81ff03d
Robert-J-WANG <robin.nz420@gmail.com>
2023-10-16 11:24:05 +1300

    08_getState_setState

--- src/stores/foodStore.ts
+++ src/stores/foodStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 
+/* 
 type TfoodStoreState = {
   fish: number;
   addOneFish: () => void;
@@ -9,16 +10,64 @@ type TfoodStoreState = {
 };
 
 export const useFoodStore = create<TfoodStoreState>()(
-  subscribeWithSelector((set) => ({
-    fish: 0,
-    addOneFish: () =>
-      set((state) => ({
-        fish: state.fish + 1,
-      })),
-    removeOneFish: () =>
-      set((state) => ({
-        fish: state.fish - 1,
-      })),
-    removeAllFish: () => set({ fish: 0 }),
-  }))
+  // 开发者工具
+  devtools(
+    // 选择器订阅
+    subscribeWithSelector(
+      // 本地存储
+      persist(
+        (set) => ({
+          fish: 0,
+          addOneFish: () =>
+            set((state) => ({
+              fish: state.fish + 1,
+            })),
+          removeOneFish: () =>
+            set((state) => ({
+              fish: state.fish - 1,
+            })),
+          removeAllFish: () => set({ fish: 0 }),
+        }),
+        { name: "food-store" }
+      )
+    ),
+    {
+      name: "food store",
+    }
+  )
+); 
+*/
+
+/* ------------------- 可以使用getState方法， ------------------ */
+/* --------------- 将state里的action函数抽离到外面， --------------- */
+/* --------------------- 让state结果更简洁 -------------------- */
+const initialState = {
+  fish: 0,
+  color: "golden",
+};
+export const useFoodStore = create<typeof initialState>()(
+  // 开发者工具
+  devtools(
+    // 选择器订阅
+    subscribeWithSelector(
+      // 本地存储
+      persist(() => initialState, { name: "food-store" })
+    ),
+    {
+      name: "food store",
+    }
+  )
 );
+
+export const addOneFish = () =>
+  useFoodStore.setState((state) => ({
+    fish: state.fish + 1,
+  }));
+export const removeOneFish = () =>
+  useFoodStore.setState((state) => ({
+    fish: state.fish - 1,
+  }));
+export const removeAllFish = () =>
+  useFoodStore.setState({
+    fish: 0,
+  });
